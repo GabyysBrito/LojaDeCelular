08bcdd3d96086b188a3069d94d87047e3596bcf7
GabyysBrito <gaby.oliveira.brito1289@gmail.com>
2025-01-20 20:59:47 -0300

    Ajustando o import do arquivo global

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/header/Header'
 import Main from './components/main/Main'
 
-import './globalStyle/globalStyle.module.scss'
+import './globalStyle.module.scss'
 
 export default function App() {
   return(
