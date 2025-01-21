5d6c8462c5d43be111874a7f5ad5e7aa8e8e6a10
GabyysBrito <gaby.oliveira.brito1289@gmail.com>
2025-01-20 21:34:33 -0300

    Renomeando o arquivo do globalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/header/Header'
 import Main from './components/main/Main'
-import './globalStyle/globalStyle.module.scss'
+import './globalStyle/globalStyle.scss'
 
 export default function App() {
   return(
